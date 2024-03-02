0258d44a032bce5de5cd8024426f05a0f4492163
halwohamda <halwohamda@gmail.com>
2024-03-01 20:50:27 -0800

    done

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-  {/* <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
@@ -34,4 +34,4 @@ const App = () => {
   )
 }
 
-export default App */}
+export default App
