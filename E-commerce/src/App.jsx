8e9cfb439efbeebdb60be34e00319ee43dfa862d
halwohamda <halwohamda@gmail.com>
2024-03-01 20:52:53 -0800

    done

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
   return (
    <div>
    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+    {/* <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div> */}
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     {/* <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div> */}
