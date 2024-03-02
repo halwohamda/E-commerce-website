5a51a369edcbaed0db6b2dd6fc3c7971a408bf14
halwohamda <halwohamda@gmail.com>
2024-03-01 20:42:24 -0800

    done

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () => {
   return (
    <div>
-    
+   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
    </div>
-    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+ 
   )
 }
 
