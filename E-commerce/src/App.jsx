1bf07f69522bb7901e302a4bfeb57bf98538a0f4
halwohamda <halwohamda@gmail.com>
2024-03-01 20:41:18 -0800

    done

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-4xl'>App</div>
+    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
   )
 }
 
