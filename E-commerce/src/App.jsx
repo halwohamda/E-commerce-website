ea1257dddf2c24e68c7422605e3ce51e29a353ee
halwohamda <halwohamda@gmail.com>
2024-03-01 20:46:45 -0800

    done

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-  //  <div>
+   <div>
    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+    {/* <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+    <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div> */}
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     {/* <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
