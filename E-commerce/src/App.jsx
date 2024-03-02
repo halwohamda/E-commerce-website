715403a013fc9f0386171bcd636e8fc6074fa5d8
halwohamda <halwohamda@gmail.com>
2024-03-01 20:47:53 -0800

    done

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -18,14 +18,14 @@ const App = () => {
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
-    // <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>   <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
+  <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
     <div className='text-4xl text-center text-cyan-50 text-nowrap'>App</div>
